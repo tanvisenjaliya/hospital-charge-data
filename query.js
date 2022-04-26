43a84dabbfbb5d7352ce0b749cc295aa625285e0
Tanvi <53575763+tanvisenjaliya@users.noreply.github.com>
2022-04-26 14:13:35 +0530

    MongoDB Query

---
+++ query.js
@@ -0,0 +1,18 @@
+db.getCollection('hop').aggregate([{$match: {'DRGDefination': {$regex: "W/O MCC"} } },{$group: {
+        _id: {
+            ProviderState: "$ProviderState",
+            ProviderCity: "$ProviderCity"
+        },       sum:{$sum:"$TotalDischarges" },
+    }
+},{    $group: {
+        _id: "$_id.ProviderState",
+        City: {
+            $push: {
+                ProviderCity: "$_id.ProviderCity",
+                TotalDischarges: $sum
+            }},	sum: {"$sum":"$sum"}
+    }
+},{ $project: {	City:{$slice: ["$City",5] },	sum:1}},
+{ $sort : { TotalDischarges: -1, sum:-1 } },
+{ $limit: 5}
+])
